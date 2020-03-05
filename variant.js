ab51ae76d7bc7459647ab3c7b21d438304709e0c
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2020-03-05 14:53:38 -0300

    Update variant.js

--- variant.js
+++ variant.js
@@ -1,45 +1,32 @@
-import checkoutHidden from '../components/checkoutHidden';
-//import Services from '../configs/services/Services';
-
 export default function variant () {    
     init();
 
-    //let oldURL = "";
+    let oldURL = "";
     let currentURL = window.location.href;
     console.log(currentURL)
 
-        //if (currentURL != oldURL) {
-            
-        if (currentURL > 0) {
+        if (currentURL != oldURL) { 
 
-            return new MutationObserver(mutations => {         
-
-            document.querySelectorAll('.checkout-content .carrinho-desktop app-carrinho .cart header').length > 0,
-              console.log("PRIMEIRO IF ADDCLASS")
+            if(document.querySelectorAll('.checkout-content .carrinho-desktop app-carrinho .cart header').length > 0) {
+                console.log("PRIMEIRO IF ADDCLASS")
             $(document.querySelector(".carrinho-desktop app-carrinho section.cart")).addClass("collapse-cro");  
+            }
 
-            document.querySelectorAll('#wrap app-checkout #pageName .row .col-md-12 app-pagamento').length > 0,
-             console.log("SEGUNDO IF REMOVECLASS")
-            $(document.querySelector('.carrinho-desktop app-carrinho section.cart')).removeClass("collapse-cro");   
+            if(window.location.pathname.indexOf("checkout") > 0 && $('.tabs div').eq(3).hasClass('active')){
+                console.log("ENTRA NO IF TABS E REMOVE A CLASSE")
+                $(document.querySelector('.carrinho-desktop app-carrinho section.cart')).removeClass("collapse-cro")
+            }           
 
-            //oldURL = currentURL;
+        oldURL = currentURL;
 
-            //oldURL = window.location.href;
+        oldURL = window.location.href;
 
-                // setTimeout(function() {
-                //     this.window.location.href;
-                // }, 1000);
-            
+            setTimeout(function() {
+                this.window.location.href;
+            }, 1000);  
+        }                
 
-            }).observe(document, {
-                childList: true,
-                subtree: true,
-                attributes: true
-            })
-        }
-
-        document.querySelector(".checkout-content .carrinho-desktop app-carrinho .cart header").insertAdjacentHTML("beforeend",'<div class="cro-icon"> </div>')          
-    
+        document.querySelector(".checkout-content .carrinho-desktop app-carrinho .cart header").insertAdjacentHTML("beforeend",'<div class="cro-icon"> </div>');    
 }
 
 
@@ -53,15 +40,10 @@ function init() {
 }
 
 
-
-// if ($(document).on("click", '#dados-pessoais-next', function (e) { 
-
-//     $(document.querySelector(".carrinho-desktop app-carrinho section.cart")).addClass("collapse-cro");
-
-// }));
-
-// if ($(document).on("click", "button[data-gtm-event-label='avancar-para-pagamento']", function (e) { 
-
-//     $(document.querySelector(".carrinho-desktop app-carrinho section.cart")).removeClass("collapse-cro");
-
-// }));
+$('body').click(function() {
+    variant()
+    $(document.querySelector('.carrinho-desktop app-carrinho section.cart')).removeClass("collapse-cro");
+    
+    
+    
+});
