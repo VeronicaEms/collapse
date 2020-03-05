90c32c2e5d409171b41bce08e0cc552a6abd7147
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2020-03-05 15:04:39 -0300

    Update variant.js

--- variant.js
+++ variant.js
@@ -1,20 +1,25 @@
 export default function variant () {    
     init();
-
+    //variável para url anterior
     let oldURL = "";
+    //variável para url que estou agora
     let currentURL = window.location.href;
     console.log(currentURL)
 
+    //se url que estou agora for diferente da url anterior
         if (currentURL != oldURL) { 
 
+            //verifica se existe o carrinho, se existir, adiciona a classe collapse-cro para FECHAR o carrinho
             if(document.querySelectorAll('.checkout-content .carrinho-desktop app-carrinho .cart header').length > 0) {
                 console.log("PRIMEIRO IF ADDCLASS")
             $(document.querySelector(".carrinho-desktop app-carrinho section.cart")).addClass("collapse-cro");  
             }
 
+            //verifica se na url de nome checkout existir e se existe a terceira aba "pagamento" com a classe active:
+            //REMOVE a classe collapse-cro tornando o carrinho ABERTO
             if(window.location.pathname.indexOf("checkout") > 0 && $('.tabs div').eq(3).hasClass('active')){
                 console.log("ENTRA NO IF TABS E REMOVE A CLASSE")
-                $(document.querySelector('.carrinho-desktop app-carrinho section.cart')).removeClass("collapse-cro")
+                $(document.querySelector('.carrinho-desktop app-carrinho section.cart')).removeClass("collapse-cro");
             }           
 
         oldURL = currentURL;
@@ -26,6 +31,7 @@ export default function variant () {
             }, 1000);  
         }                
 
+        //insere setinha do carrinho
         document.querySelector(".checkout-content .carrinho-desktop app-carrinho .cart header").insertAdjacentHTML("beforeend",'<div class="cro-icon"> </div>');    
 }
 
@@ -39,7 +45,7 @@ function init() {
     });
 }
 
-
+//quando houver um click no body, ele irá chamar a função que verifica a terceira aba(pagamento) e remove a classe.
 $('body').click(function() {
     variant()
     $(document.querySelector('.carrinho-desktop app-carrinho section.cart')).removeClass("collapse-cro");
